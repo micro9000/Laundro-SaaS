3ebb0c1a04c669a56432b8b184b22783aa5c83ec
Raniel Garcia <ranielgarcia2596@gmail.com>
2024-10-06 17:51:44 +0800

    Fix Serilog and Azure App Insights Logging

---
+++ src/Laundro/Laundro.API/Plumbing/ConfigureSerilogLogging.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Serilog;
+
+namespace Laundro.API.Plumbing;
+
+public static class ConfigureSerilogLogging
+{
+    public static IServiceCollection AddSerilogLogging(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSerilog((services, loggerConfiguration) =>
+                loggerConfiguration
+                    .ReadFrom.Configuration(configuration)
+                    .ReadFrom.Services(services)
+                    .WriteTo.Console()
+                    .WriteTo.ApplicationInsights(services.GetRequiredService<TelemetryConfiguration>(), TelemetryConverter.Traces));
+
+        return services;
+    }
+}
--- src/Laundro/Laundro.API/Program.cs
+++ src/Laundro/Laundro.API/Program.cs
@@ -1,67 +1,75 @@
 using Laundro.API.Authentication;
 using Laundro.API.Data;
 using Laundro.API.Plumbing;
-using Microsoft.ApplicationInsights.Extensibility;
 using NodaTime;
 using NodaTime.Serialization.SystemTextJson;
 using Serilog;
 using System.Text.Json;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApplicationInsightsTelemetry();
-
 Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
-                .Enrich.FromLogContext()
-                .Enrich.WithMachineName()
-                .Enrich.WithEnvironmentName()
                 .CreateBootstrapLogger();
 
-//builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
-//            .WriteTo.ApplicationInsights(
-//                    services.GetRequiredService<TelemetryConfiguration>(), TelemetryConverter.Traces));
-
-builder.AddServiceDefaults();
-
-// We can remove this if we are not going to use controllers later
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
-        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        options.JsonSerializerOptions.WriteIndented = true;
-    });
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddGlobalCorsPolicy(builder.Configuration);
-builder.Services.AddCustomNodaTimeClock();
-
-// Application components
-builder.Services.AddDatabaseStorage(builder.Configuration);
-builder.Services.AddCaching(builder.Configuration);
-builder.Services.AddRepositories();
-builder.Services.AddLaundroAzureADAuthentication(builder.Configuration);
-
-var app = builder.Build();
-
-app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddApplicationInsightsTelemetry();
+    builder.Services.AddSerilogLogging(builder.Configuration);
+
+    builder.AddServiceDefaults();
+
+    // We can remove this if we are not going to use controllers later
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.JsonSerializerOptions.WriteIndented = true;
+        });
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddGlobalCorsPolicy(builder.Configuration);
+    builder.Services.AddCustomNodaTimeClock();
+
+    // Application components
+    builder.Services.AddDatabaseStorage(builder.Configuration);
+    builder.Services.AddCaching(builder.Configuration);
+    builder.Services.AddRepositories();
+    builder.Services.AddLaundroAzureADAuthentication(builder.Configuration);
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapDefaultEndpoints();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseRouting();
+    app.UseHttpsRedirection();
+
+    app.UseGlobalCorsPolicy();
+    app.UseLaundroAzureADAuthentication();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseRouting();
-app.UseHttpsRedirection();
 
-app.UseGlobalCorsPolicy();
-app.UseLaundroAzureADAuthentication();
-
-app.MapControllers();
-
-app.Run();
