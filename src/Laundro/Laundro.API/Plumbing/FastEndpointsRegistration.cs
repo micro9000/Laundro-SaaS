0281d7b9fcd5817a29252ad1b784c510f023654f
Raniel Garcia <ranielgarcia2596@gmail.com>
2024-10-12 15:13:31 +0800

    FastEndpoints configurations

---
+++ src/Laundro/Laundro.API/Plumbing/FastEndpointsRegistration.cs
@@ -0,0 +1,39 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+using System.Text.Json;
+
+namespace Laundro.API.Plumbing;
+
+public static class FastEndpointsRegistration
+{
+    public static IServiceCollection AddFastEndpointsConfigs(
+        this IServiceCollection services)
+    {
+        services.AddFastEndpoints()
+            .ConfigureHttpJsonOptions(o =>
+            {
+                o.SerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+                o.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                o.SerializerOptions.WriteIndented = true;
+            })
+            .SwaggerDocument(o =>
+            {
+                o.DocumentSettings = s =>
+                {
+                    s.Title = "Laundro API";
+                    s.Version = "v1";
+                };
+            });
+        return services;
+    }
+
+    public static IApplicationBuilder UseFastEndpointsConfigs(this IApplicationBuilder app)
+    {
+        //app.UseDefaultExceptionHandler(); // Let's use the global exception handlers in Infrastructure dir for now
+        app.UseFastEndpoints().UseSwaggerGen();
+
+        return app;
+    }
+}
